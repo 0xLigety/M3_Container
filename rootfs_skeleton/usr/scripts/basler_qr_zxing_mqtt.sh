2908428378af9af7202e16435c18cc8bc7612271
Johannes Liegert <johannes1.liegert@st.oth-regensburg.de>
2017-10-06 12:23:44 +0200

    added loop script

--- rootfs_skeleton/usr/scripts/basler_qr_zxing_mqtt.sh
+++ rootfs_skeleton/usr/scripts/basler_qr_zxing_mqtt.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+INTERVAL=3000
 CAMERA=192.168.88.6
 ADDRESS=broker.hivemq.com
 PORT=1883
 TOPIC="/BaslerQR"
 MESSAGE="Test"
 
-mkdir /usr/application/data
+while [ 1 ]
+do
+#Grab image from camera
 /usr/application/BaslerGrabSave ${CAMERA}
+#Read QR Code from grabbed image
 MESSAGE= "$(/usr/application/zxing GrabbedImage.png)"
+#Push message to mqtt broker
 /usr/application/mosquitto_pub -h ${ADDRESS} -p ${PORT} -t ${TOPIC} -m "${MESSAGE}"
+#Sleep polling interval
+sleep ${INTERVAL}
+done
